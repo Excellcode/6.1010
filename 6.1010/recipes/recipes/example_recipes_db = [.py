e08ada4bff6419788aa1a3e822f60941703ce8f9
Excellcode <excellawusi2021@gmail.com>
2025-11-24 03:40:54 -0500

    Delete 6.1010 directory

--- 6.1010/recipes/recipes/example_recipes_db = [.py
+++
@@ -1,8 +0,0 @@
-def combinations(list):
-    input ={list[0]}
-    for num in list[1:]:
-        for input_num in input:
-            out = set()
-            
-            out.update([input_num + num, ])
-            
