cbd8aa93920d51af08cff56dfb90a08787e10d7e
Excellcode <excellawusi2021@gmail.com>
2025-11-24 03:18:05 -0500

    Initial commit with .gitignore

---
+++ 16.C20_new/excell@mit.edu_4401499_s4401499_4566804_Nov_10_2025_2-11-59am_PST/resource/startercode/gd_tests.py
@@ -0,0 +1,115 @@
+################################################################################
+# CSE.C20 Fall 2023
+# Problem Set 3: run_tests
+# Name: Excell Kumah Awusi
+# Collaborators: None
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import gd
+
+
+def J0(xv):
+    """
+    Calculates J0 objective function and its gradient (see pset desription).
+
+    Args:
+        xv (1D numpy array): for this problem, only one value (xv[0]) will be present
+
+    Returns:
+        J0 (float): objective function at x = xv[0]
+        J0prime (1D numpy array): gradient of J0 at x = xv[0]
+    """
+    x = xv[0]
+    J0 = (x-0.5)**2
+    J0prime = 2 * (x-0.5)
+    return J0, J0prime
+
+
+def J1(xv, pdict):
+    """
+    Calculates J1 objective function and its gradient (see pset desription).
+
+    Args:
+        xv (1D numpy array): x = xv[0], y = xv[1]
+        pdict (dictionary): contains the key 'user' with the associated value referring
+            to the user location such that:
+                pdict['user'][0] is the x-location of the user
+                pdict['user'][1] is the y-location of the user
+
+    Returns:
+        J1 (float): objective function at xv
+        J1prime (1D numpy array): gradient of J1 at xv
+    """
+    #### BEGIN SOLUTION ####
+    x_sub = xv[0] - pdict['user'][0]
+    y_sub = xv[1] - pdict['user'][1]
+    D = 1 + (x_sub)**2 + (y_sub)**2
+    J1 = -1/D
+    J1prime = np.zeros(2)
+    J1prime[0] = 2*x_sub/D**2
+    J1prime[1] = 2*y_sub/D**2
+    return J1, J1prime
+    #### END SOLUTION ####
+
+
+def run_test0():
+    print()
+    print("Running test0")
+    xstart = np.array([-0.5])
+    alpha = 0.1
+    nStop = 40
+    xhist, Jhist = gd.gradient_descent(J0, xstart, alpha, nStop, verbose=True)
+    print(f"In run_test0: xopt = {xhist[-1,0]:.2e}, Jmin = {Jhist[-1]:.2e}")
+
+
+def run_test1():
+    print()
+    print("Running test1")
+    pdict = {}
+    pdict['user'] = np.array([0.5, -0.25])
+    xstart = np.array([0., 0.])
+    alpha = 0.1
+    nStop = 40
+    xhist, Jhist = gd.gradient_descent(J1, xstart, alpha, nStop, verbose=True, pdict=pdict)
+    print(f"In run_test1: (xopt, yopt) = ({xhist[-1,0]:.2e}, {xhist[-1,1]:.2e}), Jmin = {Jhist[-1]:.2e}")
+
+    # Set up linearly spaced points in x and y for evaluating objective function
+    Nx = 101
+    Ny = 101
+    bx = np.linspace(-1., 1., Nx)
+    by = np.linspace(-1., 1., Ny)
+    f = np.zeros((Nx, Ny))
+
+    #### BEGIN SOLUTION ####
+    var = np.zeros(2)
+    for i in range(Nx):
+        for j in range(Ny):
+            var[0], var[1] = bx[i], by[j]
+            f[j, i] = J1(var, pdict)[0] 
+    #raise NotImplementedError("Plot contours of J1") # this is where you assign axs and cs
+    fig, axs = plt.subplots()
+    cs = axs.contour(bx, by, f)
+    axs.clabel(cs)
+    axs.set_xlabel('x')
+    axs.set_ylabel('y')
+    
+    #raise NotImplementedError("Plot xhist markers on top of J1's contours")
+    axs.plot(xhist[:-1, 0], xhist[:-1, 1], marker ='o', 
+             linestyle ='None', color = 'green')
+    axs.plot(xhist[-1, 0], xhist[-1, 1], 
+             marker='o',         
+             color='magenta')
+    axs.grid(True)
+    axs.axis('square')
+
+    #### END SOLUTION ####
+
+    return fig, axs, cs
+
+
+if __name__ == '__main__':
+    run_test0()
+    run_test1()
+    plt.show()
